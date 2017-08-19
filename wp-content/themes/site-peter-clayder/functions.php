743d7694d372013f57a1719f1533560c5d0398c8
Peter Clayder <peterclayder@gmail.com>
2017-08-19 20:38:51 -0300

    inseri o tema wordpress

---
+++ wp-content/themes/site-peter-clayder/functions.php
@@ -0,0 +1,5 @@
+<?php
+	// ativa o thumbnails
+	add_theme_support('post-thumbnails');
+	// define o tamanho do thumb
+	set_post_thumbnail_size(120, 120);
---
+++ wp-content/themes/site-peter-clayder/header-topo.php
@@ -0,0 +1,34 @@
+<header>
+	<div class="container content">
+		<div class="row">
+			<div class="col-md-9">
+				<div class="row topo">
+					<div class="col-md-12">
+						<p class="tema-site">
+							<span class="nome-topo"> Peter Clayder </span> <br/>
+							<span id="texto-topo"></span><span id="cursor"> | </span>
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<a href="https://bitbucket.org/peterclayder/" target=“_blank” class="redes-sociais"><i class="fa fa-bitbucket fa-3x" aria-hidden="true"></i></a>
+						<a href="https://t.me/peterclayder" target=“_blank” class="redes-sociais"><i class="fa fa-telegram fa-3x" aria-hidden="true"></i></a>
+						<a href="https://twitter.com/peterclayder" target=“_blank” class="redes-sociais"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+						<a href="https://www.linkedin.com/in/peter-clayder/" target=“_blank” class="redes-sociais"><i class="fa fa-linkedin fa-3x" aria-hidden="true"></i></a>
+						<a href="https://bitbucket.org/peterclayder/" target=“_blank” class="redes-sociais"><i class="fa fa-github fa-3x" aria-hidden="true"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+			</div>
+		</div>
+		<div class="row" id="animateTest">
+			<div class="col-md-9" >
+				<a  href="#apresentacao" style="position: relative; top: 20px; color: #48d09b"> <i class="fa fa-arrow-circle-down fa-3x" aria-hidden="true"></i></a>
+			</div>
+		</div>
+
+	</div>
+
+</header>
