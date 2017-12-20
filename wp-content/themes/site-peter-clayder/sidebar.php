aa52251ddfda3300b64169552dec4973e62caf6f
Clayder <peterclayder@gmail.com>
2017-12-20 02:55:15 -0200

    Modifiquei o menu lateral, retirei o bitbucket e inseri o gist.

--- wp-content/themes/site-peter-clayder/sidebar.php
+++ wp-content/themes/site-peter-clayder/sidebar.php
@@ -3,8 +3,8 @@
     <ul class="nav sidebar-nav">
         <?php imprimeMenu(); ?>
         <li><a href="mailto:peterclayder@gmail.com" target=“_blank”><i class='fa fa-envelope'></i> E-mail </a></li>
-        <li><a href="https://bitbucket.org/peterclayder/" target=“_blank”><i class='fa fa-bitbucket'></i> Bitbucket </a></li>
         <li><a href="https://www.linkedin.com/in/peter-clayder/" target=“_blank”><i class='fa fa-linkedin'></i> Linkedin </a></li>
-        <li><a href="https://bitbucket.org/peterclayder/" target=“_blank”><i class='fa fa-github'></i> Github </a></li>
+        <li><a href="https://gist.github.com/Clayder" target=“_blank”><i class='fa fa-github-alt'></i> GitHubGist </a></li>
+        <li><a href="https://github.com/Clayder" title="GitHub" target=“_blank”><i class='fa fa-github'></i> Github </a></li>
     </ul>    
 </nav>
